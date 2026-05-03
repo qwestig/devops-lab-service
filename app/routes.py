6244e6788ddf1543544acfc753ec998cc7c118e8
qwestig <levon-krtyan@mail.ru>
2026-05-03 12:04:31 +0000

    Update service response message

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ task_id = 0
 
 @router.get("/")
 def root():
-    return {"message": "Service is running"}
+    return {"message": "Service is running after CI/CD deploy"}
 
 
 @router.post("/tasks")
